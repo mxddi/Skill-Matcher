4e0d4ff41592e8b7a1620e72f4e070bf95c031f9
Julien Bertazzo Lambert <42924425+JLambertazzo@users.noreply.github.com>
2020-09-26 17:16:25 -0500

    Update __init__.py
    not using database stuff yet

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -20,13 +20,13 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
 
-    from . import db
-    db.init_app(app)
+#     from . import db
+#     db.init_app(app)
 
     # from . import auth
     # app.register_blueprint(auth.bp)
